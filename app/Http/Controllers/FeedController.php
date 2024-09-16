b3571674b3b1298a4091a183aefff9b549ae3807
DESKTOP-0G5I9E3\Osaka <fabian26-26@hotmail.com>
2024-09-16 23:17:44 +0200

    Añadida la funcionalidad del Feed y modificada la fecha de publicacion de Ideas

---
+++ app/Http/Controllers/FeedController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class FeedController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+
+        $followingsIDs = auth()->user()->followings()->pluck('user_id');
+
+        $ideas = Idea::whereIn('user_id',$followingsIDs)->latest();
+
+        // where content like %test%
+        if(request()->has('search')){
+            $ideas = $ideas->where('content','like', '%' . request()->get('search','') . '%');
+        }
+
+        return view('dashboard', [
+            'ideas' => $ideas->paginate(5)
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FollowerController;
+use App\Http\Controllers\FeedController;
 use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\IdeaLikeController;
 use App\Http\Controllers\UserController;
@@ -55,6 +56,7 @@ Route::post('users/{user}/unfollow',[FollowerController::class,'unfollow'])->mid
 Route::post('ideas/{idea}/like',[IdeaLikeController::class,'like'])->middleware('auth')->name('ideas.like');
 Route::post('ideas/{idea}/unlike',[IdeaLikeController::class,'unlike'])->middleware('auth')->name('ideas.unlike');
 
+Route::get('/feed', FeedController::class)->middleware('auth')->name('feed');
 
 Route::get('/terms', function(){
     return view('terms');
