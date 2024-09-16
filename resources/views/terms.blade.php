1dd8087b86b5100983fdc1f2910f2c51251d2642
DESKTOP-0G5I9E3\Osaka <fabian26-26@hotmail.com>
2024-09-16 17:17:51 +0200

    Se resalta tu posicion actual en los menus de navegacion

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,6 +1,26 @@
 @extends('layout.layout')
 
 @section('content')
-<h1>Terms</h1>
-<div>CUALQUIER WEA</div>
+<div class="row">
+    <div class="col-3">
+        @include('shared.left-sidebar')
+    </div>
+    <div class="col-6">
+        <h1>Terms</h1>
+        <div> Es un hecho establecido hace demasiado tiempo que un lector se
+            distraerá con el contenido del texto de un sitio mientras que mira
+            su diseño. El punto de usar Lorem Ipsum es que tiene una distribución
+            más o menos normal de las letras, al contrario de usar textos como por
+            ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo un
+            español que se puede leer. Muchos paquetes de autoedición y editores de páginas
+            web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de
+            "Lorem Ipsum" va a dar por resultado muchos sitios web que usan este texto si se encuentran
+            en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas
+            veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por
+            el estilo). </div>
+    </div>
+    <div class="col-3">
+        @include('shared.search-bar')
+        @include('shared.follow-box')
+    </div>
 @endsection
