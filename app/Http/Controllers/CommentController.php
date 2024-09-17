b573b04c41f9be4daeba72fd08abe892064d688f
DESKTOP-0G5I9E3\Osaka <fabian26-26@hotmail.com>
2024-09-17 18:20:52 +0200

    Form request añadido al proyecto

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,19 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCommentRequest;
 use App\Models\Idea;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store(Idea $idea){
+    public function store(CreateCommentRequest $request ,Idea $idea){
 
-        $comment = new Comment();
-        $comment->idea_id = $idea->id;
-        $comment->user_id = auth()->id();
-        $comment->content = request()->get('content');
-        $comment->save();
+        $validated = $request->validated();
+
+        $validated['user_id'] = auth()->id();
+        $validated['idea_id'] = $idea->id;
+
+        Comment::create($validated);
 
         return redirect()->route('ideas.show',$idea->id)->with('success',"Comment posted successfully!");
     }
