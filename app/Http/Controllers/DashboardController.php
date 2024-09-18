697bf06e7c37f08a0ed6a8fb296f8257bdd07f9b
DESKTOP-0G5I9E3\Osaka <fabian26-26@hotmail.com>
2024-09-18 18:06:10 +0200

    Top Users list con variables globales para todas las views

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Mail\WelcomeEmail;
 use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -18,6 +19,7 @@ class DashboardController extends Controller
         if(request()->has('search')){
             $ideas = $ideas->where('content','like', '%' . request()->get('search','') . '%');
         }
+
 
         return view('dashboard', [
             'ideas' => $ideas->paginate(5)
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +27,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        View::share(
+            'topUsers',
+            User::withCount('ideas')
+            ->orderBy('ideas_count','DESC')
+            ->limit(5)->get());
     }
 }
